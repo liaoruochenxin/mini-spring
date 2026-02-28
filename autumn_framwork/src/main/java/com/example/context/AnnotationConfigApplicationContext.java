78e5c7d41590d2e7c2bbd0f895f13060746c82d1
蒋炜 <2196716491@qq.com>
2026-02-28 11:23:06 +0800

    2.3 创建 BeanDefinition

--- autumn_framwork/src/main/java/com/example/context/AnnotationConfigApplicationContext.java
+++ autumn_framwork/src/main/java/com/example/context/AnnotationConfigApplicationContext.java
@@ -1,5 +1,9 @@
 package com.example.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -10,26 +14,200 @@ import java.util.function.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.annotation.Bean;
+import com.example.annotation.Component;
 import com.example.annotation.ComponentScan;
+import com.example.annotation.Configuration;
+import com.example.annotation.Import;
+import com.example.annotation.Order;
+import com.example.annotation.Primary;
+import com.example.exception.BeanCreationException;
+import com.example.exception.BeanDefinitionException;
+import com.example.exception.BeanNotOfRequiredTypeException;
 import com.example.exception.NoUniqueBeanDefinitionException;
 import com.example.io.PropertyResolver;
 import com.example.io.ResourceResolver;
 import com.example.utils.ClassUtils;
 
 import jakarta.annotation.Nullable;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class AnnotationConfigApplicationContext {
 
-    Logger logger = LoggerFactory.getLogger(getClass());
-    Map<String, BeanDefinition> beans;
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected final Map<String, BeanDefinition> beans;
+    protected final PropertyResolver propertyResolver;
+
 
     public AnnotationConfigApplicationContext(Class<?> configClass, PropertyResolver propertyResolver) {
+        this.propertyResolver = propertyResolver;
+        
         // 扫描获取所有 Bean 的 Class 类型
-        scanFor
+        final Set<String> beanClassNames = scanForClassName(configClass);
+        // 创建 Bean 的定义
+        this.beans = createBeanDefinitions(beanClassNames);
+    }
+
+    /**
+     * 根据扫描的ClassName创建BeanDefinition
+     */
+    Map<String, BeanDefinition> createBeanDefinitions(Set<String> classNameSet) {
+        Map<String, BeanDefinition> defs = new HashMap<>();
+        for (String className : classNameSet) {
+            // 获取Class:
+            Class<?> clazz = null;
+            try {
+                clazz = Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                throw new BeanCreationException(e);
+            }
+            if (clazz.isAnnotation() || clazz.isEnum() || clazz.isInterface() || clazz.isRecord()) {
+                continue;
+            }
+            // 判断是否标注@Component
+            Component component = ClassUtils.findAnnotation(clazz, Component.class);
+            if (component != null) {
+                logger.atDebug().log("found component: {}", clazz.getName());
+                int mod = clazz.getModifiers();
+                if (Modifier.isAbstract(mod)) {
+                    throw new BeanDefinitionException("@Component class " + clazz.getName() + " must not be abstract");
+                }
+                if (Modifier.isPrivate(mod)) {
+                    throw new BeanDefinitionException("@Component class " + clazz.getName() + " must not be private");
+                }
+
+                String beanName = ClassUtils.getBeanName(clazz);
+                var def = new BeanDefinition(beanName, clazz, getSuitableConstructor(clazz), getOrder(clazz), clazz.isAnnotationPresent(Primary.class), 
+                    // named init / destroy method
+                    null, 
+                    null, 
+                    ClassUtils.findAnnotationMethod(clazz, PostConstruct.class), 
+                    ClassUtils.findAnnotationMethod(clazz, PreDestroy.class));
+                addBeanDefinitions(defs, def);
+                logger.atDebug().log("define bean: {}", def);
+
+                // 如果当前类是配置类时的处理
+                Configuration configuration = ClassUtils.findAnnotation(clazz, Configuration.class);
+                if (configuration != null) {
+                    scanFactoryMethods(beanName, clazz, defs);
+                }
+            }
+        }
+        return defs;
+    }
+
+    /**
+     * 扫描带 @Bean 注解的工厂方法
+     * @param factoryBeanName
+     * @param clazz
+     * @param defs
+     */
+    void scanFactoryMethods(String factoryBeanName, Class<?> clazz, Map<String, BeanDefinition> defs) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            Bean bean = method.getAnnotation(Bean.class);
+            if (bean != null) {
+                int mod = method.getModifiers();
+                if (Modifier.isAbstract(mod)) {
+                    throw new BeanDefinitionException("@Bean method " + clazz.getName() + "." + method.getName() + " must not be abstract");
+                }
+                if (Modifier.isFinal(mod)) {
+                    throw new BeanDefinitionException("@Bean method " + clazz.getName() + "." + method.getName() + " must not be final");
+                }
+                if (Modifier.isPrivate(mod)) {
+                    throw new BeanDefinitionException("@Bean method " + clazz.getName() + "." + method.getName() + " must not be private.");
+                }
+                Class<?> beanClass = method.getReturnType();
+                if (beanClass.isPrimitive()) {
+                    throw new BeanDefinitionException("@Bean method " + clazz.getName() + "." + method.getName() + " must not return primitive type.");
+                }
+                if (beanClass == void.class || beanClass == Void.class) {
+                    throw new BeanDefinitionException("@Bean method " + clazz.getName() + "." + method.getName() + " must not return void.");
+                }
+                var def = new BeanDefinition(ClassUtils.getBeanName(method), beanClass, factoryBeanName, method, getOrder(method), 
+                    method.isAnnotationPresent(Primary.class), 
+                    // init method
+                    bean.initMethod().isEmpty() ? null : bean.initMethod(),
+                    // destroy method
+                     bean.destoryMethod().isEmpty() ? null : bean.destoryMethod(), 
+                     // @PostConstruct / @PreDestroy method:
+                     null, null);
+                addBeanDefinitions(defs, def);
+                logger.atDebug().log("define bean: {}", def);
+            }
+        }
+    }
+
+    /**
+     * 将 BeanDefinition 安全加入到 Map 中
+     * @param defs 存放 beans 的 Map
+     * @param def 需要放入的 BeanDefinition
+     */
+    void addBeanDefinitions(Map<String, BeanDefinition> defs, BeanDefinition def) {
+        if (defs.put(def.getName(), def) != null) {
+            throw new BeanDefinitionException("Duplicate bean name: " + def.getName());
+        }
+    }
+
+    /**
+     * Get public constructor or non-public constructor as fallback.
+     * 获取对应类的构造器(优先public)
+     * @param clazz 待获取构造器的类
+     * @return 构造器
+     */
+    Constructor<?> getSuitableConstructor(Class<?> clazz) {
+        // 获取 public 的构造器
+        Constructor<?>[] cons = clazz.getConstructors();
+        if (cons.length == 0) {
+            // 获取所有访问控制级别的构造器
+            cons = clazz.getDeclaredConstructors();
+            if (cons.length != 1) {
+                throw new BeanDefinitionException("More than one constructor found in class " + clazz.getName() + ".");
+            }
+        }
+        if (cons.length != 1) {
+            throw new BeanDefinitionException("More than one constructor found in class " + clazz.getName() + ".");
+        }
+        return cons[0];
+    }
+
+    /**
+     * 获取对应类上 Order注解的值，若没有order注解则返回int类型最大值
+     * @param clazz 需要获取order注解的类
+     * @return
+     */
+    int getOrder(Class<?> clazz) {
+        Order annotation = clazz.getAnnotation(Order.class);
+        return annotation == null ? Integer.MAX_VALUE : annotation.value();
+    }
+
+    /**
+     * 获取方法上 @Order 注解的值
+     * @param method 
+     * @return
+     */
+    int getOrder(Method method) {
+        Order order = method.getAnnotation(Order.class);
+        return order == null ? Integer.MAX_VALUE : annotation.value();
+    }
+
     // 根据 name 查找 BeanDefinition，如果不存在返回 null
     @Nullable
     public BeanDefinition findBeanDefinition(String name) {
         return this.beans.get(name);
+    }
+
+    public BeanDefinition findBeanDefinition(String name, Class<?> requiredType) {
+        BeanDefinition def = findBeanDefinition(name);
+        if (def == null) {
+            return null;
+        }
+        if (!requiredType.isAssignableFrom(def.getBeanClass())) {
+            throw new BeanNotOfRequiredTypeException(String.format("Autowire required type '%s' but bean '%s' has actual type '%s'.", requiredType.getName(),
+                    name, def.getBeanClass().getName()));
+        }
+        return def;
     }
 
     /**
@@ -46,6 +224,11 @@ public class AnnotationConfigApplicationContext {
                 .sorted().toList();
     }
 
+    /**
+     * 根据 Type 查找某个 BeanDefinition,如果不存在则返回 null.如果存在多个则返回 @Primary 标注的那个
+     * @param type
+     * @return
+     */
     public BeanDefinition findBeanDefinition(Class<?> type) {
         List<BeanDefinition> defs = findBeanDefinitions(type);
         if (defs.isEmpty()) { // 未找到对应类型的Bean
@@ -85,7 +268,7 @@ public class AnnotationConfigApplicationContext {
             logger.atDebug().log("scan package: {}", pkg);
             // 扫描一个包
             var rr = new ResourceResolver(pkg);
-            List<String> ClassList = rr.scan(res -> {
+            List<String> classList = rr.scan(res -> {
                 // 遇到以 .class 结尾的文件，就将其转换为 Class 全名:
                 String name = res.name();
                 if (name.endsWith(".class")) {
@@ -93,6 +276,37 @@ public class AnnotationConfigApplicationContext {
                 }
                 return null;
             });
+            if (logger.isDebugEnabled()) {
+                classList.forEach((className) -> {
+                    logger.debug("class found by component scan: {}", className);
+                });
+            }
+            classNameSet.addAll(classList);
+
         }
+
+        // 查找 @Import(Xyz.class)
+        Import importConfig = configClass.getAnnotation(Import.class);
+        if (importConfig != null) {
+            for (Class<?> importConfigClass : importConfig.value()) {
+                String importClassName = importConfigClass.getName();
+                if (classNameSet.contains(importClassName)) {
+                    logger.warn("ignore import: " + importClassName + " for it is already been scanned.");
+                } else {
+                    logger.debug("class found by importer: {}", importClassName);
+                    classNameSet.add(importClassName);
+                }
+            }
+        }
+        return classNameSet;
+    }
+
+    /**
+     * 判断是否有@Configuration注解
+     * @param def 需要判断的类
+     * @return 是否有 @Configuration 注解
+     */
+    boolean isConfigurationDefinition(BeanDefinition def) {
+        return ClassUtils.findAnnotation(def.getBeanClass(), Configuration.class) != null;
     }
 }
--- autumn_framwork/src/main/java/com/example/utils/ClassUtils.java
+++ autumn_framwork/src/main/java/com/example/utils/ClassUtils.java
@@ -1,8 +1,15 @@
 package com.example.utils;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
+import com.example.annotation.Bean;
+import com.example.annotation.Component;
 import com.example.exception.BeanDefinitionException;
+
+import jakarta.annotation.Nullable;
 
 /**
  * 判断是否存在 @Component ，不但要在当前类上查找，也要在当前类所有注解上，查找该注解是否有 @Component
@@ -27,5 +34,88 @@ public class ClassUtils {
             }
         }
         return a;
+    }
+
+    /**
+     * get bean name by:
+     * 
+     * 获取带 @Component 注解的 Bean 的名称
+     * <code>
+     * @Component
+     * public class Hello {}
+     * </code>
+     * @param clazz
+     * @return
+     */
+    public static String getBeanName(Class<?> clazz) {
+        String name = "";
+        // 查找@Component
+        Component component = clazz.getAnnotation(Component.class);
+        if (component != null) {
+            // Component exist:
+            name = component.value();
+        } else {
+            // 未找到@Component, 继续在其他注解中查找@Component:
+            for(Annotation anno : clazz.getAnnotations()) {
+                if (findAnnotation(anno.annotationType(), null) != null) {
+                    try {
+                        name = (String) anno.annotationType().getMethod("value").invoke(anno);
+                    } catch (ReflectiveOperationException e) {
+                        throw new BeanDefinitionException("Cannot get annotation value.", e);
+                    }
+                }
+            }
+        }
+        if (name.isEmpty()) {
+            // default name : "HelloWold => helloWorld"
+            name = clazz.getSimpleName();
+            name = Character.toLowerCase(name.charAt(0)) + name.substring(1);
+        }
+        return name;
+    }
+
+    /**
+     * Get bean name by:
+     * 
+     * 获取 @Bean 注解标注的 Bean 名称
+     * <code>
+     * @Bean
+     * Hello createHello() {}
+     * </code>
+     */
+    public static String getBeanName(Method method) {
+        Bean bean = method.getAnnotation(Bean.class);
+        String name = bean.value();
+        if (name.isEmpty()) {
+            name = method.getName();
+        }
+        return name;
+    }
+
+    /**
+     * 获取指定类中无参的，有指定注解的方法
+     * @param clazz 指定类
+     * @param annoClass 指定注解类
+     * @return 符合条件的方法
+     */
+    @Nullable
+    public static Method findAnnotationMethod(Class<?> clazz, Class<? extends Annotation> annoClass) {
+        // try get declared method:
+        List<Method> ms = Arrays.stream(clazz.getDeclaredMethods()).filter(
+            m -> m.isAnnotationPresent(annoClass)
+        ).map(
+            m -> {
+                if (m.getParameterCount() != 0) {
+                    throw new BeanDefinitionException(String.format("Method '%s' with @%s must not have argument: %s ", m.getName(), annoClass.getSimpleName(), clazz.getName()));
+                }
+                return m;
+            }).toList();
+        if (ms.isEmpty()) {
+            return null;
+        }
+        if (ms.size() == 1) {
+            return ms.get(0);
+        }
+        throw new BeanDefinitionException(String.format("Multiple methods with @%s found in class: %s", annoClass.getSimpleName(), clazz.getName()));
     }
 }
